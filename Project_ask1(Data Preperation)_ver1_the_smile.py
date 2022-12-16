8e957e2a4d724052eed995a0dce3f0eccbc2e59c
Orestis Makris <79021400+OrestisMakris@users.noreply.github.com>
2022-12-16 19:42:12 +0200

    Update Project_ask1(Data Preperation)_ver1_the_smile.py

--- Project_ask1(Data Preperation)_ver1_the_smile.py
+++ Project_ask1(Data Preperation)_ver1_the_smile.py
@@ -25,19 +25,18 @@ Patient_data[10].replace(2 ,1,inplace=True)
 
 Patient_data=Patient_data.dropna()
 
-# fig,axes = plt.subplots(1 ,12 ,sharey=True)
-# sns.histplot(Patient_data , ax =axes[0] , x=0 , kde=True )
-# sns.histplot(Patient_data , ax =axes[1] , x=1 , kde=True )
-# sns.histplot(Patient_data , ax =axes[2] , x=2 , kde=True , color = 'b')
-# sns.histplot(Patient_data , ax =axes[3] , x=3 , kde=True , color = 'g')
-# sns.histplot(Patient_data , ax =axes[4] , x=4 , kde=True , color = 'c')
-# sns.histplot(Patient_data , ax =axes[5] , x=5 , kde=True )
-# sns.histplot(Patient_data , ax =axes[6] , x=6 , kde=True ,color = 'g')
-# sns.histplot(Patient_data , ax =axes[7] , x=7 , kde=True ,color = 'g')
-# sns.histplot(Patient_data , ax =axes[8] , x=8 , kde=True , color = 'b')
-# sns.histplot(Patient_data , ax =axes[9] , x=9 , kde=True , color = 'g')
-# sns.histplot(Patient_data , ax =axes[10] , x=10, kde=True , color = 'c')
-# #plt.show()
+fig,axes = plt.subplots(1 ,12 ,sharey=True)
+sns.histplot(Patient_data , ax =axes[0] , x=0 , kde=True )
+sns.histplot(Patient_data , ax =axes[1] , x=1 , kde=True )
+sns.histplot(Patient_data , ax =axes[2] , x=2 , kde=True , color = 'b')
+sns.histplot(Patient_data , ax =axes[3] , x=3 , kde=True , color = 'g')
+sns.histplot(Patient_data , ax =axes[4] , x=4 , kde=True , color = 'c')
+sns.histplot(Patient_data , ax =axes[5] , x=5 , kde=True )
+sns.histplot(Patient_data , ax =axes[6] , x=6 , kde=True ,color = 'g')
+sns.histplot(Patient_data , ax =axes[7] , x=7 , kde=True ,color = 'g')
+sns.histplot(Patient_data , ax =axes[8] , x=8 , kde=True , color = 'b')
+sns.histplot(Patient_data , ax =axes[9] , x=9 , kde=True , color = 'g')
+sns.histplot(Patient_data , ax =axes[10] , x=10, kde=True , color = 'c')
 
 Patient_data[0] = np.log(Patient_data[0])
 
@@ -46,42 +45,42 @@ for i in range(2,10):
 
 
 
-# fig,axes = plt.subplots(1 ,12 ,sharey=True)
-# sns.histplot(Patient_data , ax =axes[0] , x=0 , kde=True )
-# sns.histplot(Patient_data , ax =axes[1] , x=1 , kde=True )
-# sns.histplot(Patient_data , ax =axes[2] , x=2 , kde=True , color = 'b')
-# sns.histplot(Patient_data , ax =axes[3] , x=3 , kde=True , color = 'g')
-# sns.histplot(Patient_data , ax =axes[4] , x=4 , kde=True , color = 'c')
-# sns.histplot(Patient_data , ax =axes[5] , x=5 , kde=True )
-# sns.histplot(Patient_data , ax =axes[6] , x=6 , kde=True ,color = 'g')
-# sns.histplot(Patient_data , ax =axes[7] , x=7 , kde=True ,color = 'g')
-# sns.histplot(Patient_data , ax =axes[8] , x=8 , kde=True , color = 'b')
-# sns.histplot(Patient_data , ax =axes[9] , x=9 , kde=True , color = 'g')
-# sns.histplot(Patient_data , ax =axes[10] , x=10, kde=True , color = 'c')
-# #plt.show()
+fig,axes = plt.subplots(1 ,12 ,sharey=True)
+sns.histplot(Patient_data , ax =axes[0] , x=0 , kde=True )
+sns.histplot(Patient_data , ax =axes[1] , x=1 , kde=True )
+sns.histplot(Patient_data , ax =axes[2] , x=2 , kde=True , color = 'b')
+sns.histplot(Patient_data , ax =axes[3] , x=3 , kde=True , color = 'g')
+sns.histplot(Patient_data , ax =axes[4] , x=4 , kde=True , color = 'c')
+sns.histplot(Patient_data , ax =axes[5] , x=5 , kde=True )
+sns.histplot(Patient_data , ax =axes[6] , x=6 , kde=True ,color = 'g')
+sns.histplot(Patient_data , ax =axes[7] , x=7 , kde=True ,color = 'g')
+sns.histplot(Patient_data , ax =axes[8] , x=8 , kde=True , color = 'b')
+sns.histplot(Patient_data , ax =axes[9] , x=9 , kde=True , color = 'g')
+sns.histplot(Patient_data , ax =axes[10] , x=10, kde=True , color = 'c')
+
 
 corr = Patient_data.iloc[:,:-1].corr(method='pearson')
 cmap = sns.diverging_palette(250,354,80,60 , center='dark' , as_cmap=True)
 sns.heatmap(corr , vmax =1 , vmin =.3 , cmap=cmap ,  annot=True,square =True , linewidths = .2)
-plt.show()
+
 
 Patient_data[0] = 2*((Patient_data[0] - min(Patient_data[0])) / ( max(Patient_data[0]) - min(Patient_data[0]) ))-1
 for i in range(2,10):
     Patient_data[i] = 2*((Patient_data[i] - min(Patient_data[i])) / ( max(Patient_data[i]) - min(Patient_data[i]) ))-1
 
-# fig,axes = plt.subplots(1 ,11 ,sharey=True)
-# sns.histplot(Patient_data , ax =axes[0] , x=0 , kde=True )
-# sns.histplot(Patient_data , ax =axes[1] , x=1 , kde=True )
-# sns.histplot(Patient_data , ax =axes[2] , x=2 , kde=True , color = 'b')
-# sns.histplot(Patient_data , ax =axes[3] , x=3 , kde=True , color = 'g')
-# sns.histplot(Patient_data , ax =axes[4] , x=4 , kde=True , color = 'c')
-# sns.histplot(Patient_data , ax =axes[5] , x=5 , kde=True )
-# sns.histplot(Patient_data , ax =axes[6] , x=6 , kde=True ,color = 'g')
-# sns.histplot(Patient_data , ax =axes[7] , x=7 , kde=True ,color = 'g')
-# sns.histplot(Patient_data , ax =axes[8] , x=8 , kde=True , color = 'b')
-# sns.histplot(Patient_data , ax =axes[9] , x=9 , kde=True , color = 'g')
-# sns.histplot(Patient_data , ax =axes[10] , x=10, kde=True , color = 'c')
-# #plt.show()
+fig,axes = plt.subplots(1 ,11 ,sharey=True)
+sns.histplot(Patient_data , ax =axes[0] , x=0 , kde=True )
+sns.histplot(Patient_data , ax =axes[1] , x=1 , kde=True )
+sns.histplot(Patient_data , ax =axes[2] , x=2 , kde=True , color = 'b')
+sns.histplot(Patient_data , ax =axes[3] , x=3 , kde=True , color = 'g')
+sns.histplot(Patient_data , ax =axes[4] , x=4 , kde=True , color = 'c')
+sns.histplot(Patient_data , ax =axes[5] , x=5 , kde=True )
+sns.histplot(Patient_data , ax =axes[6] , x=6 , kde=True ,color = 'g')
+sns.histplot(Patient_data , ax =axes[7] , x=7 , kde=True ,color = 'g')
+sns.histplot(Patient_data , ax =axes[8] , x=8 , kde=True , color = 'b')
+sns.histplot(Patient_data , ax =axes[9] , x=9 , kde=True , color = 'g')
+sns.histplot(Patient_data , ax =axes[10] , x=10, kde=True , color = 'c')
+plt.show()
 
 
 Patient_data_Y = Patient_data.iloc[:,10:]
@@ -98,13 +97,36 @@ y_pred = classifier.predict(Patient_data_X_test)
 ac = accuracy_score(Patient_data_Y_test ,y_pred)
 cm = confusion_matrix(Patient_data_Y_test , y_pred)
 
-cv = KFold(n_splits=10, random_state=1, shuffle=True)
-
 print (ac)
 print(cm)
+
+cv = KFold(n_splits=5, random_state=1, shuffle=True)
 
 # evaluate model
 scores = cross_val_score(classifier, Patient_data_X, Patient_data_Y, scoring='accuracy', cv=cv, n_jobs=-1)
 # report performance
 print(scores)
 
+#-------------------------------------------------------
+
+Patient_data_X_rd = Patient_data_X[[0,1,4 ]]
+#Patient_data_X_rd = Patient_data_X[[0,1]]
+Patient_data_X_train_rd, Patient_data_X_test_rd, Patient_data_Y_train_rd, Patient_data_Y_test_rd = train_test_split(Patient_data_X_rd, Patient_data_Y, test_size=0.2,random_state=218)
+
+classifier_reduced_dependency =  GaussianNB()
+classifier_reduced_dependency.fit(Patient_data_X_train_rd, Patient_data_Y_train_rd)
+
+y_pred_rd = classifier_reduced_dependency.predict(Patient_data_X_test_rd)
+
+ac_rd = accuracy_score(Patient_data_Y_test_rd ,y_pred_rd)
+cm_rd = confusion_matrix(Patient_data_Y_test_rd, y_pred_rd)
+
+print (ac_rd)
+print(cm_rd)
+
+cv_rd = KFold(n_splits=5, random_state=1, shuffle=True)
+
+# evaluate model
+scores_rd = cross_val_score(classifier_reduced_dependency, Patient_data_X_rd, Patient_data_Y, scoring='accuracy', cv=cv, n_jobs=-1)
+# report performance
+print(scores_rd)
